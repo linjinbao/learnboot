e68ebcd5c0f877c524e073099dc8a0ae1e41a6da
linjb <linjb@fline88.com>
2020-02-25 21:35:41 +0800

    jdbctemplate

---
+++ learnJDBCTemplate/src/main/java/tk/amrom/learnjdbc/IAccountDAO.java
@@ -0,0 +1,17 @@
+package tk.amrom.learnjdbc;
+
+import tk.amrom.learnjdbc.Account;
+
+import java.util.List;
+
+public interface IAccountDAO {
+    int add(Account account);
+
+    int update(Account account);
+
+    int delete(int id);
+
+    Account findAccountById(int id);
+
+    List<Account> findAccountList();
+}
---
+++ learnJDBCTemplate/src/main/java/tk/amrom/learnjdbc/IAccountService.java
@@ -0,0 +1,16 @@
+package tk.amrom.learnjdbc;
+
+import java.util.List;
+
+public interface IAccountService {
+    int add(Account account);
+
+    int update(Account account);
+
+    int delete(int id);
+
+    Account findAccountById(int id);
+
+    List<Account> findAccountList();
+
+}
