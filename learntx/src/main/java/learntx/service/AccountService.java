1b59ecc840c92ec705b69b86208ec1aee239ed5d
linjb <linjb@fline88.com>
2020-02-27 22:53:22 +0800

    事务

---
+++ learntx/src/main/java/learntx/service/AccountService.java
@@ -0,0 +1,32 @@
+package learntx.service;
+
+import learntx.dao.AccountMapper;
+import learntx.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class AccountService {
+    @Autowired
+    private AccountMapper accountMapper;
+
+    public int add(String name, double money) {
+        return accountMapper.add(name, money);
+    }
+    public int update(String name, double money, int id) {
+        return accountMapper.update(name, money, id);
+    }
+    public int delete(int id) {
+        return accountMapper.delete(id);
+    }
+    public Account findAccount(int id) {
+        return accountMapper.findAccount(id);
+    }
+    public List<Account> findAccountList() {
+        return accountMapper.findAccountList();
+    }
+}
