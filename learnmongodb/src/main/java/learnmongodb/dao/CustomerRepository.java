a50fd92ad0e0f8944bfd5edde28b33ee488275c0
linjb <linjb@fline88.com>
2020-02-28 15:53:04 +0800

    learn mongodb

---
+++ learnmongodb/src/main/java/learnmongodb/dao/CustomerRepository.java
@@ -0,0 +1,11 @@
+package learnmongodb.dao;
+
+import learnmongodb.entity.Customer;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface CustomerRepository extends MongoRepository<Customer, String> {
+    public Customer findByFirstName(String firstName);
+    public List<Customer> findByLastName(String lastName);
+}
