a5a612d14ee20e499bba51a0d86443c49b19a275
linjb <linjb@fline88.com>
2020-03-01 20:43:19 +0800

    spring redis 订阅发布/

---
+++ learnredis/src/main/java/learnredis/message/Receiver.java
@@ -0,0 +1,34 @@
+package learnredis.message;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * @author linjinbao66@gmail.com
+ * date 2020-03-01
+ * @apiVersion
+ * @apiGroup
+ * @apiDescription
+ * @apiParam
+ * @apiParamExample
+ * @apiSuccess (200)
+ * @apiSuccessExample
+ */
+public class Receiver {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Receiver.class);
+
+    private CountDownLatch latch;
+
+    @Autowired
+    public Receiver(CountDownLatch latch) {
+        this.latch = latch;
+    }
+
+    public void receiveMessage(String message) {
+        LOGGER.info("Received <" + message + ">");
+        latch.countDown();
+    }
+}
